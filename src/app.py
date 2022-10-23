0d3ad4b3c6e5d854225fa074b618c507b84c947b
eymcmanus <eymcmanus@gmail.com>
2022-10-23 18:38:25 -0500

    Output health

--- src/app.py
+++ src/app.py
@@ -6,6 +6,8 @@ from fastapi import FastAPI
 
 # Local application imports
 from .routes import group
+from .routes import health
 
 app = FastAPI()
 app.include_router(group.router)
+app.include_router(health.router)
