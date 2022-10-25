83e1b7de6f48ebd942445736ea74d55b05f02854
tg2648 <45052145+tg2648@users.noreply.github.com>
2022-10-25 10:41:34 -0400

    Add EB deployment support

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn src.app:app --worker-class=uvicorn.workers.UvicornWorker
---
+++ src/routes/health.py
@@ -0,0 +1,31 @@
+# Standard library imports
+from typing import List
+from datetime import datetime
+
+# Third party imports
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+# Local application imports
+from ..db.base import get_db
+from ..schema.group import GroupDto, GroupCreateDto
+from ..crud import group as group_crud
+
+
+router = APIRouter()
+
+
+@router.get("/")
+async def get_whatever():
+    return "hello"
+
+
+@router.get("/api/health")
+async def get_health():
+    t = str(datetime.now())
+    msg = {
+        "name": "Groups Api",
+        "health": "Good",
+        "at time": t
+    }
+    return msg
