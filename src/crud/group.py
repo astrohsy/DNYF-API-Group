aeb8f45896659916b72ae32e059a03e218b55d7f
eymcmanus <77636303+eymcmanus@users.noreply.github.com>
2022-10-28 19:57:41 -0400

    Database (#3)
    * Output health
    
    * support for deleting group
    
    * Support for PUT
    
    * creation of members table
    
    * Support for adding members to a group
    
    * Support for removing member from a group
    
    * Support for getting all members in a group
    
    * Add links to member response
    
    * Added group links
    
    * Response from PUT now includes relative links but DELETE will not
    
    Co-authored-by: eymcmanus <eymcmanus@gmail.com>

--- src/crud/group.py
+++ src/crud/group.py
@@ -6,8 +6,8 @@ from sqlalchemy.orm import Session
 
 # Local application imports
 from ..db.group import Group
-from ..schema.group import GroupCreateDto
-
+from ..db.members import Members, Association
+from ..schema.group import GroupCreateDto, GroupBaseDto
 
 def get_group(db: Session, group_id: int) -> Union[Group, None]:
     return db.query(Group).filter(Group.group_id == group_id).first()
@@ -16,10 +16,49 @@ def get_group(db: Session, group_id: int) -> Union[Group, None]:
 def get_groups(db: Session, offset: int = 0, limit: int = 10) -> List[Group]:
     return db.query(Group).offset(offset).limit(limit).all()
 
+def get_members(group_id: int, db: Session, offset: int = 0, limit: int = 10) -> List[Members]:
+    assoc = db.query(Association).filter(Association.group_id == group_id).offset(offset).limit(limit).all()
+    return assoc
+
 
 def create_group(db: Session, group: GroupCreateDto) -> Group:
     db_group = Group(**group.dict())
     db.add(db_group)
     db.commit()
     db.refresh(db_group)
+    return db_group
+
+def delete_group(db: Session, group_id: int) -> Union[Group, None]: 
+    db_group = db.query(Group).filter(Group.group_id == group_id).first()
+    deleted_group = db_group
+    db.delete(db_group)
+    db.commit()
+    db.refresh
+    return deleted_group
+
+def put_groupname(new_group: dict, db: Session,  group_id: int) -> Union[Group, None]: 
+    db.query(Group).filter(Group.group_id == group_id).update(new_group, synchronize_session="fetch")
+    db.commit()
+    db.refresh
+    db_group = db.query(Group).filter(Group.group_id == group_id).first()
+    return db_group
+
+def add_member(new_member: dict, db: Session,  group_id: int) -> Union[Group, None]: 
+    #db.query(Group).filter(Group.group_id == group_id).update(new_group, synchronize_session="fetch")
+    assoc = Association(**new_member)
+    db.add(assoc)
+    db.commit()
+    db.refresh
+    db_group = db.query(Group).filter(Group.group_id == group_id).first()
+    return db_group
+
+def delete_member(db: Session, group_id:int, member_id:int) -> Union[Group, None]: 
+    del_assoc = db.query(Association).filter(
+        Association.group_id == group_id, 
+        Association.member_id == member_id
+        ).first()
+    db.delete(del_assoc)
+    db.commit()
+    db.refresh
+    db_group = db.query(Group).filter(Group.group_id == group_id).first()
     return db_group
--- src/routes/group.py
+++ src/routes/group.py
@@ -3,6 +3,7 @@ User endpoint routing
 """
 # Standard library imports
 from typing import List
+from datetime import datetime
 
 # Third party imports
 from fastapi import APIRouter, Depends, HTTPException
@@ -10,15 +11,13 @@ from sqlalchemy.orm import Session
 
 # Local application imports
 from ..db.base import get_db
-from ..schema.group import GroupDto, GroupCreateDto
+from ..schema.group import GroupDto, GroupCreateDto, MemberDto
 from ..crud import group as group_crud
-
 
 router = APIRouter(
     prefix='/groups',
     tags=['groups']
 )
-
 
 @router.get("/", response_model=List[GroupDto])
 def read_groups(offset: int = 0, limit: int = 100,
@@ -36,7 +35,45 @@ def read_group(group_id: int, db: Session = Depends(get_db)):
 
     return db_group
 
+@router.get("/{group_id}/members", response_model=List[MemberDto])
+def get_members(group_id: int, offset: int = 0, limit: int = 100,
+                db: Session = Depends(get_db)):
+    members = group_crud.get_members(group_id=group_id, offset=offset, limit=limit, db=db)
+    return members
+
 
 @router.post("/", response_model=GroupDto)
 def create_group(group: GroupCreateDto, db: Session = Depends(get_db)):
     return group_crud.create_group(db=db, group=group)
+
+
+@router.post("/{group_id}/members", response_model=GroupDto)
+def add_member(new_member: dict, group_id:int, db: Session = Depends(get_db)):
+    db_group = group_crud.get_group(db, group_id=group_id)
+    if db_group is None:
+        raise HTTPException(status_code=404, detail="Group not found")
+
+    return group_crud.add_member(db=db, new_member=new_member, group_id=group_id)
+
+
+@router.delete("/{group_id}")
+def delete_group(group_id: int, db: Session = Depends(get_db), response_model=GroupDto):
+    #raise an error here 
+    db_group = group_crud.get_group(db, group_id=group_id)
+    if db_group is None:
+        raise HTTPException(status_code=404, detail="Group not found")
+
+    return group_crud.delete_group(db=db, group_id=group_id)
+
+
+@router.delete("/{group_id}/members/{member_id}")
+def delete_member(group_id: int, member_id:int, db: Session = Depends(get_db)):
+    #raise an error here 
+    return group_crud.delete_member(db=db, group_id=group_id, member_id=member_id)
+    
+@router.put("/{group_id}", response_model=GroupDto)
+def put_groupname(new_group: dict, group_id: int, db: Session = Depends(get_db)):
+    #raise an error here 
+    return group_crud.put_groupname(new_group=new_group, db=db,  group_id=group_id)
+
+
