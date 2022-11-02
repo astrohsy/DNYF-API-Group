813d28d8a32e2d90382c2d31984781a76e23a250
tg2648 <45052145+tg2648@users.noreply.github.com>
2022-11-02 16:12:02 -0400

    Add sample data

--- src/app.py
+++ src/app.py
@@ -4,13 +4,19 @@ Main API entrypoint
 # Third party imports
 from fastapi import FastAPI
 
+
 # Local application imports
 from .routes import group
 from .routes import health
 from .db.base import engine, Base
+from .sample_data import add_sample_data
 
+# Create all DB schema from scratch on every startup
 Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
+
+# Add sample data to DB
+add_sample_data()
 
 app = FastAPI()
 app.include_router(group.router)
---
+++ src/sample_data.py
@@ -0,0 +1,21 @@
+# Local application imports
+from .schema.group import GroupCreateDto
+from .db.base import SessionLocal
+from .crud import group as group_crud
+
+
+sample_groups = [
+    {"group_name": "OS Study Group", "group_capacity": 4},
+    {"group_name": "Learning Java", "group_capacity": 2},
+    {"group_name": "Algo Midterm", "group_capacity": 3},
+    {"group_name": "DB Project", "group_capacity": 4},
+    {"group_name": "6156 Project", "group_capacity": 5},
+    {"group_name": "PLT Peer Programming", "group_capacity": 2},
+]
+
+
+def add_sample_data():
+    db = SessionLocal()
+    for group in sample_groups:
+        group_crud.create_group(db=db, group=GroupCreateDto(**group))
+    db.close()
