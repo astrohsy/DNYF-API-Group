3260c7aba91d972032b33b1414d865ec65c95dcd
eymcmanus <eymcmanus@gmail.com>
2022-11-12 14:27:21 -0500

    Filter by multiple query parameters

--- src/crud/group.py
+++ src/crud/group.py
@@ -14,12 +14,14 @@ def get_group(db: Session, group_id: int) -> Union[Group, None]:
     return db.query(Group).filter(Group.group_id == group_id).first()
 
 
-def count_groups(db: Session) -> int:
-    return db.query(Group).count()
+def count_groups(db: Session, params: dict = None) -> int:
+    return db.query(Group).filter_by(**params).count()
 
 
-def get_groups(db: Session, offset: int = 0, limit: int = 10) -> List[Group]:
-    return db.query(Group).offset(offset).limit(limit).all()
+def get_groups(
+    db: Session, offset: int = 0, limit: int = 10, params: dict = None
+) -> List[Group]:
+    return db.query(Group).filter_by(**params).offset(offset).limit(limit).all()
 
 
 def get_members(group_id: int, db: Session) -> List[Members]:
